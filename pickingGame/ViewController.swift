d9e06561617392465cbed4fd4412dbbfb5385ca3
Zharkynuly Yedil <62800476+yed1l@users.noreply.github.com>
2020-11-04 19:42:41 +0600

    Swift
    first commit

---
+++ pickingGame/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  pickingGame
+//
+//  Created by Yedil on 10/7/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func FlipCard(_ sender: UIButton) {
+    }
+
+
+}
+
